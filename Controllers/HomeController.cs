29732afabc8f56d7ebb5399a5cd6253aa3a5ed69
usamabintahir99 <67357499+usamabintahir99@users.noreply.github.com>
2020-06-24 10:27:55 +0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PTS.Models;
+
+namespace PTS.Controllers
+{
+    public class HomeController : Controller
+    {
+        PrayersTimingClassDataContext dc = new PrayersTimingClassDataContext();
+        public ActionResult Index()
+        {
+            ViewBag.msg = TempData["msg"];
+            return View(dc.PrayersTimings.Select(m=>m.Area).Distinct().ToList());
+        }
+
+        public ActionResult Category()
+        {
+            var areaName = Request["cat"];
+            areaName.Replace("%20"," ");
+            return View(dc.PrayersTimings.Where(a=>a.Area==areaName));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(dc.PrayersTimings.First(m=>m.Id==id));
+        }
+
+        public ActionResult EditDone(int id)
+        {
+            var v = dc.PrayersTimings.First(m => m.Id == id);
+            v.Name= Request["MosqueName"];
+            v.Area=Request["Area"];
+            v.Address= Request["Address"];
+            v.Fajar= Request["Fajar-Hours"] +":"+ Request["Fajar-Minutes"] +" "+ Request["Fajar-Meridiem"];
+            v.Zuhar= Request["Zuhar-Hours"] + ":" + Request["Zuhar-Minutes"] + " " + Request["Zuhar-Meridiem"];
+            v.Asar=Request["Asar-Hours"] + ":" + Request["Asar-Minutes"] + " " + Request["Asar-Meridiem"];
+            v.Maghrib= Request["Magrib-Hours"] + ":" + Request["Magrib-Minutes"] + " " + Request["Magrib-Meridiem"];
+            v.Isha=Request["Isha-Hours"] + ":" + Request["Isha-Minutes"] + " " + Request["Isha-Meridiem"];
+            v.Jumma = Request["Jumma-Hours"] + ":" + Request["Jumma-Minutes"] + " " + Request["Jumma-Meridiem"];
+            dc.SubmitChanges();
+            TempData["msg"] = "Edited successfully.";
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var v = dc.PrayersTimings.First(m => m.Id == id);
+            dc.PrayersTimings.DeleteOnSubmit(v);
+            dc.SubmitChanges();
+            TempData["msg"] = "Deleted successfully.";
+            return RedirectToAction("Index");
+        }
+        
+        public ActionResult AddMosque()
+        {
+            ViewBag.Message = "Your add mosque page.";
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            string Name = Request["MosqueName"];
+            string Area = Request["Area"];
+            string Address = Request["Address"];
+            string Fajar = Request["Fajar-Hours"] +":"+ Request["Fajar-Minutes"] +" "+ Request["Fajar-Meridiem"];
+            string Zuhar = Request["Zuhar-Hours"] + ":" + Request["Zuhar-Minutes"] + " " + Request["Zuhar-Meridiem"];
+            string Asar = Request["Asar-Hours"] + ":" + Request["Asar-Minutes"] + " " + Request["Asar-Meridiem"];
+            string Magrib = Request["Magrib-Hours"] + ":" + Request["Magrib-Minutes"] + " " + Request["Magrib-Meridiem"];
+            string Isha = Request["Isha-Hours"] + ":" + Request["Isha-Minutes"] + " " + Request["Isha-Meridiem"];
+            string Jumma = Request["Jumma-Hours"] + ":" + Request["Jumma-Minutes"] + " " + Request["Jumma-Meridiem"];
+            PrayersTiming m = new PrayersTiming();
+            m.Address = Address;
+            m.Area = Area;
+            m.Name = Name;
+            m.Fajar = Fajar;
+            m.Zuhar = Zuhar;
+            m.Asar = Asar;
+            m.Maghrib = Magrib;
+            m.Isha = Isha;
+            m.Jumma = Jumma;
+
+            dc.PrayersTimings.InsertOnSubmit(m);
+            dc.SubmitChanges();
+            TempData["msg"] = "Mosque added successfully.";
+        
+            return RedirectToAction("Index");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Prayer-Timing-System
+# Enterprise-Application-Development
+# Project Title: Prayer Schedule Management Website
+# Introduction:
+This is my Enterprise Application Development Project/Assignment. It is a beginner level/ initial version of Prayers Timings/Schedule Management System Website build with ASP.NET MVC Framework in C#.NET (Razor Syntax). Linq to Sql and Ajax is used in this implementation.
+# Functions:
+1. View Mosque Prayer Schedule according to area.
+2. Add Mosque, its prayer schedule, area etc.
+3. Edit Mosque Name, Area, Prayer Schedule details etc.
+4. Delete a Mosque.
+# Prerequisites:
+1. Visual Studio 2013 (or above version).
+2. ASP.net MVC module (for Visual Studio).
+# Compatibility:
+This website is compatible with any modern browser (IE, Chrome, MS Edge, Firefox etc.) with V8 javascript engine.
+# Build With:
+- Visual Studio 2017
+- ASP.NET MVC Framework in C#.NET (Razor Syntax)
+# Author:
+Usama Bin Tahir (BSEF17A508)
+# Acknowledgements:
+**Course Instructor:** Waqas Riaz
